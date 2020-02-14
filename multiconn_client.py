9581ba30c7e3657bab4cb47affda39973c445772
Tiffany Tate <tptate@buffalo.edu>
2020-02-13 20:12:02 -0500

    refractored to use classes

--- multiconn_client.py
+++ multiconn_client.py
@@ -3,37 +3,45 @@ import socket
 import threading
 from _thread import *
 
-
-host = 'localhost'
-port = 65432
-
-CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-CLIENT.connect((host, port))
-
-
-def send():
-    while True:
-        send_message = input()
-        if send_message != "end":
-            CLIENT.send(send_message.encode("utf8"))
-        else:
-            CLIENT.send(send_message.encode("utf8"))
-            CLIENT.close()
-            break
+class Client(object):
+    def __init__(self):
+        self.host = 'localhost'
+        self.port = 65432
+        self.CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.CLIENT.connect((self.host, self.port))
+        self._start()
 
 
-def recieve():
-    while True:
-        try:
-            recv_message = CLIENT.recv(1024).decode("utf8")
-            print(recv_message)
-        except OSError:
-            break
+    def _start(self):
+        send_thread = threading.Thread(target=self._send)
+        send_thread.start()
+        recvieve_thread = threading.Thread(target=self._recieve)
+        recvieve_thread.start()
+
+
+
+    def _send(self):
+        while True:
+            
+            send_message = input("--> ")
+            if send_message != "end":
+                self.CLIENT.send(send_message.encode("utf-8"))
+            else:
+                self.CLIENT.send(send_message.encode("utf-8"))
+                self.CLIENT.close()
+                break
+
+
+    def _recieve(self):
+        while True:
+            try:
+                recv_message = self.CLIENT.recv(1024).decode("utf-8")
+                print(recv_message)
+            except OSError:
+                break
 
 
 if __name__ == '__main__':
+    client = Client()
 
-    recvieve_thread = threading.Thread(target=recieve)
-    recvieve_thread.start()
-    send_thread = threading.Thread(target=send)
-    send_thread.start()
+
